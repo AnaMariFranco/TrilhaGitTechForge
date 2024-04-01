842aa5b4a0cb1288854c064595fb938ea034ca06
AnaMariFranco <161780634+AnaMariFranco@users.noreply.github.com>
2024-04-01 20:57:49 -0300

    Add files via upload

---
+++ primeiroAtv.js
@@ -0,0 +1,8 @@
+const prompt = require ("prompt-sync")();
+
+let a,b;
+a= Number (prompt("Informe o valor de A: "));
+b= Number (prompt("Informe o valor de B: "));
+
+console.log(Number (a)+ Number(b), "essa é a soma");
+
